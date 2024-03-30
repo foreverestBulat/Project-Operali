a5d50f6c93aac0850b7a32c6213a8f6c412bf89d
foreverestBulat <113981993+foreverestBulat@users.noreply.github.com>
2024-03-31 00:54:31 +0300

    created base folders for architecture

---
+++ Project/Architecture.Application/Entities/Event.cs
@@ -0,0 +1,17 @@
+﻿using Architecture.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Application.Entities
+{
+    public class Event : BaseAuditableEntity
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime DateTime { get; set; }
+        public virtual List<Member> CastMembers { get; set; }
+    }
+}
---
+++ Project/Architecture.Application/Entities/Member.cs
@@ -0,0 +1,15 @@
+﻿using Architecture.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Application.Entities
+{
+    public class Member : BaseAuditableEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
